c8d69a69667c58b2baac8d8df38a42bd965aa109
hamednourhani <info@itstar.ir>
2016-07-13 11:43:26 +0430

    message source added

--- src/main/java/ir/itstar/qurickPoll/QuickPollApplication.java
+++ src/main/java/ir/itstar/qurickPoll/QuickPollApplication.java
@@ -1,16 +1,46 @@
 package ir.itstar.qurickPoll;
 
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
-public class QuickPollApplication {
+public class QuickPollApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuickPollApplication.class, args);
 	}
 	
+
+	
+	@Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+ 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+ 
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    
+    
+    
 //	@Bean
 //	public ServletRegistrationBean h2servletRegistration() {
 //	    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
@@ -18,5 +48,12 @@ public class QuickPollApplication {
 //	    return registration;
 //	}
 	
+// @Bean
+//	 public ResourceBundleMessageSource messageSource() {
+//		  ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+//		  source.setUseCodeAsDefaultMessage(true);
+//		  source.setBasenames(new String[]{"i18n/messages","i18n/ValidationMessages"});
+//		  return source;
+//	 }
 
 }
--- src/main/java/ir/itstar/qurickPoll/controller/PollController.java
+++ src/main/java/ir/itstar/qurickPoll/controller/PollController.java
@@ -1,6 +1,9 @@
 package ir.itstar.qurickPoll.controller;
 
 import java.net.URI;
+import java.util.ArrayList;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import ir.itstar.qurickPoll.domain.Poll;
+import ir.itstar.qurickPoll.exception.ResourceNotFoundException;
 import ir.itstar.qurickPoll.repository.PollRepository;
 
 @RestController
@@ -24,36 +28,51 @@ public class PollController {
 	
 	@RequestMapping(value="/polls",method=RequestMethod.GET)
 	public ResponseEntity<Iterable<Poll>> getAllPolls(){
+		Iterable<Poll> polls = pollRepository.findAll();
+		if(((ArrayList<Poll>)polls).isEmpty()){
+			throw new ResourceNotFoundException("there is no poll.");
+		}
 		return new ResponseEntity<>(pollRepository.findAll(),HttpStatus.OK);
 	}
 	
 	@RequestMapping(value="/polls",method=RequestMethod.POST)
-	public ResponseEntity<?> createPOll(@RequestBody Poll poll){
-			poll = pollRepository.save(poll);
+	public ResponseEntity<?> createPOll(@Valid @RequestBody Poll poll){
+		
+		poll = pollRepository.save(poll);
 			URI location = ServletUriComponentsBuilder
 								.fromCurrentRequest()
 								.path("/{id}").buildAndExpand(poll.getId()).toUri();
 								
 			HttpHeaders headers = new HttpHeaders();
 			headers.setLocation(location);
+			
 			return new ResponseEntity<>(null,headers,HttpStatus.CREATED);
 	}
 	
 	@RequestMapping(value="/polls/{pollId}",method=RequestMethod.GET)
 	public ResponseEntity<Poll> getPoll(@PathVariable long pollId){
+		verfiyPoll(pollId);
 		return new ResponseEntity<Poll>(pollRepository.findOne(pollId),HttpStatus.OK);
 	}
 	
 	@RequestMapping(value="/polls/{pollId}",method=RequestMethod.PUT)
 	public ResponseEntity<?> updatePoll(@PathVariable long pollId, @RequestBody Poll poll){
-		poll = pollRepository.save(poll);
-		return new ResponseEntity<>(poll, HttpStatus.OK);
+		verfiyPoll(pollId);
+		return new ResponseEntity<>(pollRepository.save(poll), HttpStatus.OK);
 	}
 	
 	@RequestMapping(value="/polls/{pollId}",method=RequestMethod.DELETE)
 	public ResponseEntity<?> deletePoll(@PathVariable long pollId){
+		verfiyPoll(pollId);
 		pollRepository.delete(pollId);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	public void verfiyPoll(long pollId) {
+		Poll poll = pollRepository.findOne(pollId);
+		if(poll == null){
+			throw new ResourceNotFoundException("The Poll with Id "+ pollId +" is not exists");
+		}
 	}
 	
 }
