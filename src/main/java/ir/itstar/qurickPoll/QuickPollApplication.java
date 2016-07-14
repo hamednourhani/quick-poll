5c179ae885a5e2c9c8179185ff7af2cc6c97f127
hamednourhani <info@itstar.ir>
2016-07-14 10:56:23 +0430

    Swagger Added

--- src/main/java/ir/itstar/qurickPoll/QuickPollApplication.java
+++ src/main/java/ir/itstar/qurickPoll/QuickPollApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 public class QuickPollApplication extends WebMvcConfigurerAdapter {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(QuickPollApplication.class, args);
 	}
--- src/main/java/ir/itstar/qurickPoll/QuickPollConfig.java
+++ src/main/java/ir/itstar/qurickPoll/QuickPollConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -32,6 +33,18 @@ public class QuickPollConfig extends WebMvcConfigurerAdapter {
 	public void addInterceptors(InterceptorRegistry registry) {
     	registry.addInterceptor(localeChangeInterceptor());
 	}
+    
+    @Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+        
+    	
+    	registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+	}
+    
     
     
 //	@Bean
